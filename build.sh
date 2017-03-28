38066d590758ad278b25a8f079c87a8cfa8eea25
Aaditya Kalsi <akalsi.dev@gmail.com>
2017-03-27 23:06:54 -0400

    Change default build to RelWithDebInfo

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 exec=`realpath $0`
 root=`dirname $exec`
 
-cmake -H$root -B$root/_build -DCMAKE_INSTALL_PREFIX=$root/_install -DCMAKE_BUILD_TYPE=Debug
+cmake -H$root -B$root/_build -DCMAKE_INSTALL_PREFIX=$root/_install -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build $root/_build --target check
 cmake --build $root/_build --target install
